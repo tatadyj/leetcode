00d0b3ef860a613636951b50d024c2dbf68e09b0
tata <tatadingyajun@hotmail.com>
2024-11-18 15:46:12 -0800

    add

--- 340.longest-substring-with-at-most-k-distinct-characters/340.longest-substring-with-at-most-k-distinct-characters.py
+++ 340.longest-substring-with-at-most-k-distinct-characters/340.longest-substring-with-at-most-k-distinct-characters.py
@@ -1,25 +1,29 @@
-from collections import defaultdict 
+#
+# @lc app=leetcode id=340 lang=python3
+#
+# [340] Longest Substring with At Most K Distinct Characters
+#
 
+# @lc code=start
 class Solution:
     def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
-        res = 0
+        res = 0 
         left = 0 
         window = defaultdict(int)
-        
+
         for right in range(len(s)):
             rval = s[right]
-            # update window
-            window[rval] += 1
-            
-            while len(window) > k:
+            window[rval] += 1 
+
+            while len(window) > k: 
                 lval = s[left]
-                left += 1 
-                
-                # update window
-                window[lval] -= 1
+                window[lval] -= 1 
                 if window[lval] == 0:
                     window.pop(lval)
-                    
+                left += 1 
+
             res = max(res, right - left + 1)
-            
-        return res 
+        return res
+        
+# @lc code=end
+
--- 992.subarrays-with-k-different-integers/992.subarrays-with-k-different-integers.py
+++ 992.subarrays-with-k-different-integers/992.subarrays-with-k-different-integers.py
@@ -5,34 +5,27 @@
 #
 
 # @lc code=start
-from collections import defaultdict
-
-def atMost(nums, k):
-    res = 0 
-    left = 0 
-    window = defaultdict(int)
-
-    for right in range(len(nums)):
-        rval = nums[right]
-        # update window
-        window[rval] += 1
-
-        while len(window) > k:
-            lval = nums[left]
-            left += 1
-            # update window
-            window[lval] -= 1
-            if window[lval] == 0:
-                window.pop(lval)
-
-        res += right - left + 1
-
-    return res 
-
-
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
-        return atMost(nums, k) - atMost(nums, k-1)
+        def at_most_k(nums, k):
+            res = 0 
+            left = 0 
+            window = defaultdict(int)
+            for right in range(len(nums)):
+                rval = nums[right]
+                window[rval] += 1 
+
+                while len(window) > k:
+                    lval = nums[left]
+                    window[lval] -= 1
+                    if window[lval] == 0:
+                        window.pop(lval)
+                    left += 1 
+                
+                res += right - left + 1
+            return res 
+
+        return at_most_k(nums, k) - at_most_k(nums, k-1)
         
 # @lc code=end
 
